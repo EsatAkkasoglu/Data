dbb35a046f8f3b3cd5a58d919f9b52539170a5be
Esat Akkaşoğlu <esatakkasoglu@ogr.iu.edu.tr>
2022-04-27 20:41:58 +0300

    Create imdb_top1000.py

---
+++ imdb_top1000.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import pandas as pd 
+film = pd.read_csv("https://raw.githubusercontent.com/justmarkham/pandas-videos/master/data/imdb_1000.csv")
+pd.read_csv("https://raw.githubusercontent.com/justmarkham/pandas-videos/master/data/imdb_1000.csv")
+plt.style.use("Solarize_Light2")
+bins = np.arange(50,260,15)
+plt.xticks(bins)
+plt.hist(film.duration, edgecolor="black", color="w" ,bins=bins, histtype='bar' )
+plt.title("imdb film süreleri")
+plt.xlabel("Süreler")
+plt.ylabel("aynı süreye sahip film sayısı") 
+
+
+
